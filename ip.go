1b5bf516b90b87ddb875eda31b9d586a5d686276
James <jismith1989@googlemail.com>
2019-09-26 14:39:26 +0100

    First commit; working project

---
+++ ip.go
@@ -0,0 +1,17 @@
+package ip
+
+import "net/http"
+
+func Get(r http.Request) (s string) {
+	s = r.Header.Get("X-Real-Ip")
+
+	if s == "" {
+		s = r.Header.Get("X-Forwarded-For")
+	}
+
+	if s == "" {
+		s = r.RemoteAddr
+	}
+
+	return s
+}
