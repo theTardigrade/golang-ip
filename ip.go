b3381ef09846b11934c40392662e4de5919a9eff
James <jismith1989@googlemail.com>
2019-10-05 13:30:18 +0100

    Improve code readability

--- ip.go
+++ ip.go
@@ -18,13 +18,14 @@ func Get(r *http.Request) (s string) {
 
 	if s != "" {
 		splitS := strings.Split(s, "::")
-		splitSLastIndex := len(splitS) - 1
 
-		if splitSLastIndex >= 0 {
+		if splitSLastIndex := len(splitS) - 1; splitSLastIndex > 0 {
 			splitSplitS := strings.Split(splitS[splitSLastIndex], ":")
+
 			if splitSplitSLastIndex := len(splitSplitS) - 1; splitSplitSLastIndex > 0 {
 				splitSplitS = splitSplitS[:splitSplitSLastIndex]
 			}
+
 			splitS[splitSLastIndex] = strings.Join(splitSplitS, ":")
 		}
 
